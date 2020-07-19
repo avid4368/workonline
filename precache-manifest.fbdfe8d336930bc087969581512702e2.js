fe43ea318995e5182b3f6503c99a8b70bf9f667b
EnigmIt <avid.ali.sh@gmail.com>
2020-07-19 10:58:43 +0530

    Updates

---
+++ precache-manifest.fbdfe8d336930bc087969581512702e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932dc7f7ddfe70d0056d04312415bdda",
+    "url": "/workonline/index.html"
+  },
+  {
+    "revision": "7e7db5f377e0d5b3c846",
+    "url": "/workonline/static/css/2.1d984fbf.chunk.css"
+  },
+  {
+    "revision": "3dbdf82a50e241d8c51c",
+    "url": "/workonline/static/css/main.1edf4fa1.chunk.css"
+  },
+  {
+    "revision": "7e7db5f377e0d5b3c846",
+    "url": "/workonline/static/js/2.01bc8e69.chunk.js"
+  },
+  {
+    "revision": "3dbdf82a50e241d8c51c",
+    "url": "/workonline/static/js/main.18cd0f3a.chunk.js"
+  },
+  {
+    "revision": "a81519403da7975caf49",
+    "url": "/workonline/static/js/runtime~main.79f8a0d2.js"
+  },
+  {
+    "revision": "2ab67eec1a3ab14d3cc6fc3eaa072181",
+    "url": "/workonline/static/media/ag-index-background.2ab67eec.png"
+  },
+  {
+    "revision": "0629e278cc0f9cf52df5ec27d4f403cb",
+    "url": "/workonline/static/media/ag-logo.0629e278.png"
+  },
+  {
+    "revision": "8504fae6a8575e0a9786f1c67bd05e69",
+    "url": "/workonline/static/media/icons.8504fae6.eot"
+  },
+  {
+    "revision": "bc005709a21d5a2af39b0744935ace95",
+    "url": "/workonline/static/media/icons.bc005709.woff"
+  },
+  {
+    "revision": "efe053d475acd04c59283fc07f718371",
+    "url": "/workonline/static/media/icons.efe053d4.ttf"
+  }
+]);
